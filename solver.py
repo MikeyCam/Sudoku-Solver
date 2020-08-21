92d249c69ee8b7106d0216a2a81ccbe69084fd89
Michael Camden-Smith <mcamdensmith@gmail.com>
2020-08-21 16:24:43 +0200

    Added random puzzle generator

--- solver.py
+++ solver.py
@@ -1,51 +1,79 @@
 import numpy as np
+import random
 
 
-board = np.array([[0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0]])
-
-board = np.array(board)
-
-def possible(row, col, n):  
-    row_array = board[row] # Use row number to return the row array
-    column_array = board[:, col] # Use column number to return the column array
+def possible(row, col, n, board):
+    row_array = board[row]  # Use row number to return the row array
+    # Use column number to return the column array
+    column_array = board[:, col]
     # Find the block array based off the corner values in the above two lines of code
     block_array = board[int((row // 3) * 3):
-                            int((row // 3) * 3) + 3, 
-                       int((col // 3) * 3):
-                            int((col // 3) * 3) + 3]
-    block_array = block_array.flatten() # Flatten 3x3 array so it is the same size as the other arrays 
-    all_numbers_array = np.stack([row_array, column_array, block_array]) # Stack arrays on top of each other
-    unique_numbers_array = np.unique(all_numbers_array) # Return unique numbers only
-    available_boolean = n not in unique_numbers_array # check whether a given number is in the unique numbers array       
+                        int((row // 3) * 3) + 3,
+                        int((col // 3) * 3):
+                        int((col // 3) * 3) + 3]
+    # Flatten 3x3 array so it is the same size as the other arrays
+    block_array = block_array.flatten()
+    # Stack arrays on top of each other
+    all_numbers_array = np.stack([row_array, column_array, block_array])
+    unique_numbers_array = np.unique(
+        all_numbers_array)  # Return unique numbers only
+    # Check whether a given number is in the unique numbers array
+    available_boolean = n not in unique_numbers_array
     return available_boolean
 
-def solve():
+
+def solve(board):
     for row in range(0, 9):
-        for col in range(0,9):
-            if(board[row,col] == 0):
+        for col in range(0, 9):
+            if(board[row, col] == 0):
                 for n in range(1, 10):
-                    if(possible(row, col, n) == True):
-                        board[row,col] = n
-                        # print(board)
+                    if(possible(row, col, n, board) == True):
+                        board[row, col] = n
                         if (board != 0).all():
                             raise StopIteration
-                        solve()
-                        board[row,col] = 0
+                        solve(board)
+                        board[row, col] = 0
                 return
 
-def solver():
-  try:
-      solve()
-  except StopIteration:
-      board
-  return board
 
-print(solver())
+def solver(board):
+    try:
+        solve(board)
+    except StopIteration:
+        board
+    return board
+
+
+def random_board(board, total_blanks):
+
+    # Input random values
+    board[0, 0] = np.random.randint(low=1, high=9, dtype="int")
+    board[1, 3] = np.random.randint(low=1, high=9, dtype="int")
+    board[2, 6] = np.random.randint(low=1, high=9, dtype="int")
+    board[3, 1] = np.random.randint(low=1, high=9, dtype="int")
+    board[4, 4] = np.random.randint(low=1, high=9, dtype="int")
+    board[5, 7] = np.random.randint(low=1, high=9, dtype="int")
+    board[6, 2] = np.random.randint(low=1, high=9, dtype="int")
+    board[7, 5] = np.random.randint(low=1, high=9, dtype="int")
+    board[8, 8] = np.random.randint(low=1, high=9, dtype="int")
+
+    solved_board = solver(board)
+
+    indicies = []
+    for x in range(0,9):
+        for y in range(0,9):
+            index = [x, y]
+            indicies.append(index)
+
+    randoms_needed = random.sample(indicies, total_blanks)
+    for r,c in randoms_needed:
+        board[r, c]= 0
+
+    return solved_board
+
+
+board = np.full((9, 9), 0)
+
+board = random_board(board,8)
+    
+print(board)
