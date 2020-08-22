df4cddad5e3753e1205879cfef486a805ed04a6b
Michael Camden-Smith <mcamdensmith@gmail.com>
2020-08-22 15:36:35 +0200

    Added conditional formatting and create puzzle

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ import dash_html_components as html
 import pandas as pd
 import numpy as np
 from styling_items import StylingItems
+from solver import random_board
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+app.title = 'Sudoku Solver'
 
 
 app.layout = html.Div(
@@ -22,22 +24,24 @@ app.layout = html.Div(
 
 
 @app.callback(
-    dash.dependencies.Output('slider-output-container', 'children'),
-    [dash.dependencies.Input('my-slider', 'value')])
-def update_output(value):
-    print("Slider set to {}".format(value))
+    Output(component_id='my_table', component_property='data'), 
+    [
+        Input("create-button", "n_clicks"), 
+        Input('my-slider', 'value')
+    ]
+)
+def update_output(data, value):
+    blank_array = np.full((9, 9), 0)
+    data = random_board(blank_array, value)
+    return data
+
 
 @app.callback(
-    Output("create-button-hidden-div", "children"), [Input("create-button", "n_clicks")]
+    Output("solve-button-hidden-div",
+           "children"), [Input("solve-button", "n_clicks")]
 )
 def on_button_click(value):
-        print("create_button_pushed {}".format(value))
-
-@app.callback(
-    Output("solve-button-hidden-div", "children"), [Input("solve-button", "n_clicks")]
-)
-def on_button_click(value):
-        print("solve_button_pushed".format(value))
+    print("solve_button_pushed".format(value))
 
 
 if __name__ == "__main__":
