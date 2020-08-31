828aad6966f26c2602da3a2fe151e85f7a6dd1af
Michael Camden-Smith <mcamdensmith@gmail.com>
2020-08-31 22:34:37 +0200

    Revised

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # sudoku_solver
 
-A Sudoku Solver built in Python using mostly Numpy array methods.
-The interface was built in Dash.
+A Sudoku Solver and Builder built in Python using mostly Numpy array methods.
+
 
--- app.py
+++ app.py
@@ -1,48 +1,20 @@
-import dash
-import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
-import dash_table
-import dash_core_components as dcc
-import dash_html_components as html
-import pandas as pd
-import numpy as np
-from styling_items import StylingItems
 from solver import random_board
+Revifrom solver import solver
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
-app.title = 'Sudoku Solver'
-
-
-app.layout = html.Div(
-    children=[
-        html.Div(StylingItems.navbar),
-        html.Div(StylingItems.jumbotron),
-        html.Div(StylingItems.selection_row),
-        html.Div(StylingItems.editable_table),
-    ]
-)
+NUMBER_OF_BLANKS = 20
 
 
-@app.callback(
-    Output(component_id='my_table', component_property='data'), 
-    [
-        Input("create-button", "n_clicks"), 
-        Input('my-slider', 'value')
-    ]
-)
-def update_output(data, value):
-    blank_array = np.full((9, 9), 0)
-    data = random_board(blank_array, value)
-    return data
+puzzle = random_board(NUMBER_OF_BLANKS)
+print("---------------------------------------")
+print("Creating Sudoku Puzzle with {} blanks.".format(str(NUMBER_OF_BLANKS)))
+print("---------------------------------------")
+print(puzzle)
+print("---------------------------------------")
+
+solved_puzzle = solver(puzzle)
+print("Solved Sudoku Puzzle.".format(str(NUMBER_OF_BLANKS)))
+print("---------------------------------------")
+print(solved_puzzle)
+print("---------------------------------------")
 
 
-@app.callback(
-    Output("solve-button-hidden-div",
-           "children"), [Input("solve-button", "n_clicks")]
-)
-def on_button_click(value):
-    print("solve_button_pushed".format(value))
-
-
-if __name__ == "__main__":
-    app.run_server(debug=True)
